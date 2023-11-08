022bda391db40c150baee8522250750e0226c8a8
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-11-08 17:18:28 +0600

    Vercel config setup

--- index.js
+++ index.js
@@ -187,10 +187,6 @@ async function run() {
             res.send(result)
         })
 
-
-
-
-
         await client.connect();
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
